aeca773db2d0501ae74eac7d795d543c5cf30bb6
amishPro <45467562+amishPro@users.noreply.github.com>
2019-11-26 08:16:54 -0300

    Add files via upload

---
+++ precache-manifest.821ae46772613eaebd6c30c2d68bd9f7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232e31eb52f9a8c60e057c0f7de51bcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "163e40a400394b775c00",
+    "url": "/static/css/main.a78a2747.chunk.css"
+  },
+  {
+    "revision": "8cdc257d791944deb483",
+    "url": "/static/js/2.427a326e.chunk.js"
+  },
+  {
+    "revision": "163e40a400394b775c00",
+    "url": "/static/js/main.d990c1e2.chunk.js"
+  },
+  {
+    "revision": "c1c836a210b0fcf77deb",
+    "url": "/static/js/runtime-main.b2fbae26.js"
+  },
+  {
+    "revision": "124666d72a564c40ec481b12a15f24c8",
+    "url": "/static/media/01.124666d7.jpg"
+  },
+  {
+    "revision": "394c12c33f7e6ed012f7db869b0d49aa",
+    "url": "/static/media/01.394c12c3.ttf"
+  },
+  {
+    "revision": "68ce48d4835f24a3881e0884007216cf",
+    "url": "/static/media/01.68ce48d4.mp4"
+  },
+  {
+    "revision": "16ac9f530adaccdbcd21b09543efc769",
+    "url": "/static/media/02.16ac9f53.jpg"
+  },
+  {
+    "revision": "1ed2a4feb70be8d870d09be6048630bf",
+    "url": "/static/media/02.1ed2a4fe.ttf"
+  },
+  {
+    "revision": "46b4bc4a77386a60b72d0cb8ac301b51",
+    "url": "/static/media/03.46b4bc4a.jpg"
+  },
+  {
+    "revision": "a49ae7d6e5dae592b6853530040bdd07",
+    "url": "/static/media/04.a49ae7d6.jpg"
+  },
+  {
+    "revision": "343a0cf98d72cd6ca26e54397ed560da",
+    "url": "/static/media/05.343a0cf9.jpg"
+  },
+  {
+    "revision": "e0f828bb2f74b7ee283093c9abacbdd9",
+    "url": "/static/media/flecha.e0f828bb.svg"
+  },
+  {
+    "revision": "920f082b9f249749752b21c0775ada73",
+    "url": "/static/media/fondochica.920f082b.jpg"
+  },
+  {
+    "revision": "058d963b47045288795d41defe50f3cc",
+    "url": "/static/media/middleLogo.058d963b.webp"
+  },
+  {
+    "revision": "6e0ee21e0e2bc46c684a4e0b96e13e25",
+    "url": "/static/media/premio_abc.6e0ee21e.svg"
+  },
+  {
+    "revision": "eaa6b7582b5d3c80b5daa1a97afa1145",
+    "url": "/static/media/premio_awwwards.eaa6b758.svg"
+  },
+  {
+    "revision": "6fcccf49c0b37f3e5cc0b43be28044fb",
+    "url": "/static/media/premio_cnet.6fcccf49.svg"
+  },
+  {
+    "revision": "53e90924e95ee292ece28835dfec1c20",
+    "url": "/static/media/premio_cnn.53e90924.svg"
+  },
+  {
+    "revision": "3bb4a83306c9f879eebcbd877ec92072",
+    "url": "/static/media/premio_css.3bb4a833.svg"
+  },
+  {
+    "revision": "788f2ec74243784c4c2719e480a75d3c",
+    "url": "/static/media/premio_elmundo.788f2ec7.svg"
+  },
+  {
+    "revision": "86daa569fc3a271a82ec49ba23a9a89c",
+    "url": "/static/media/premio_expansion.86daa569.svg"
+  },
+  {
+    "revision": "d5d8ce8c14c8511918a9b882b963f912",
+    "url": "/static/media/premio_fwa.d5d8ce8c.svg"
+  },
+  {
+    "revision": "074e26c407f48d0e6915c72e1aa4dbf2",
+    "url": "/static/media/premio_lavanguardia.074e26c4.svg"
+  },
+  {
+    "revision": "df0b7ceabc100fc11fcfc1b7aebd6443",
+    "url": "/static/media/premio_lifehacker.df0b7cea.svg"
+  },
+  {
+    "revision": "a07db2605e743f2f6f74d46a6c4d8a28",
+    "url": "/static/media/premio_rtve.a07db260.svg"
+  },
+  {
+    "revision": "c6d6a71133a749b4eba3d8662eadd387",
+    "url": "/static/media/premio_vocento.c6d6a711.svg"
+  },
+  {
+    "revision": "c4b0ab10d3dcb1ffa7555869a6b44928",
+    "url": "/static/media/starbucks.c4b0ab10.jpg"
+  }
+]);
